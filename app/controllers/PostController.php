789e3c28bc3b19c5fea3a04dc37f52016517036b
Andrea Pollitt <andrea.pollitt@gmail.com>
2014-03-01 14:27:43 -0500

    missed a file

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -2,14 +2,22 @@
 
 class PostController extends BaseController
 {
-    public function __construct(\Blog\PostRepository $repo)
+    public function __construct(\Blog\Post $post)
     {
-        $this->repo = $repo;
+        $this->post = $post;
+    }
+
+    public function updatePost()
+    {
+        $input = Input::get("post_content");
+        $this->post->update(Input::get('id'), $input);
     }
 
     public function getPosts()
     {
-        $posts = $this->repo->getPosts();
+
+        $posts = $this->post->getPosts();
+        //$posts = $this->repo->getPosts();
         return View::make('posts')->with('posts', $posts);
     }
 }
