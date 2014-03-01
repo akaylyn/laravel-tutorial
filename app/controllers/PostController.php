36d80b62ed7830ec9d0a25bf51b2af6f775f3fe4
Andrea Pollitt <andrea.pollitt@gmail.com>
2014-03-01 10:32:02 -0500

    create post repository

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -2,17 +2,16 @@
 
 class PostController extends BaseController
 {
-    public function __construct()
+    public function __construct(\Blog\PostRepository $repo)
     {
+        $this->repo = $repo;
     }
 
     public function getPosts()
     {
         // TODO:
         // you can't test this, how do you get around that?
-        $posts = Post::join('users', 'posts.author_id', '=', 'users.id')
-            ->select('title', 'users.name as author_name', 'users.email as author_email', 'posts.id', 'posts.created_at')
-            ->get();
+        $posts = $this->repo->getPosts();
         return View::make('posts')->with('posts', $posts);
     }
 }
--- app/src/PostRepository.php
+++ app/src/PostRepository.php
@@ -1,7 +1,16 @@
 <?php
+namespace Blog;
 
 // TODO: interface
 class PostRepository
 {
+    public function getPosts()
+    {
+        $posts = \Post::join('users', 'posts.author_id', '=', 'users.id')
+            ->select('title', 'users.name as author_name', 'users.id as author_id',
+                'posts.id', 'posts.created_at')
+            ->get();
+        return $posts;
+    }
 }
 
